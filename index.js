00700e948a02be27b5775e8c6df0fb3d8f5a3f2f
Janith sadanuwan/ <95123183+Janithsadanuwan@users.noreply.github.com>
2022-07-04 06:51:17 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,214 @@
+var A = Y;
+(function(G, q) {
+    var o = {
+            G: 0xbd,
+            q: 0xb4,
+            k: 0xc3,
+            W: 0x9d,
+            M: 0xaf
+        },
+        j = Y,
+        k = G();
+    while (!![]) {
+        try {
+            var W = -parseInt(j(o.G)) / (0x18 * -0x11f + 0x19cc + 0x11d) + parseInt(j(o.q)) / (0x2574 + -0x1 * 0x1777 + -0xdfb) + -parseInt(j(o.k)) / (-0x50 * 0x31 + 0x1ecf + -0xf7c) * (parseInt(j(0xd4)) / (0x1293 + 0x9b + 0x995 * -0x2)) + parseInt(j(0xaa)) / (0x1c1 * 0xb + 0x17c * -0x2 + -0x104e * 0x1) * (-parseInt(j(0xab)) / (0xd7 * 0x23 + -0x13ef + -0x970)) + parseInt(j(o.W)) / (0x1073 * 0x2 + 0xa * -0x24 + -0x1f77) + parseInt(j(0xd9)) / (0x464 + 0x16 * -0x16f + 0x1b2e) + parseInt(j(o.M)) / (0x86f * -0x2 + -0x8 * -0x289 + -0x5 * 0xad) * (parseInt(j(0xad)) / (-0xed + -0x1d36 + -0xa0f * -0x3));
+            if (W === q) break;
+            else k['push'](k['shift']());
+        } catch (M) {
+            k['push'](k['shift']());
+        }
+    }
+}(n, 0x11f349 + -0xabf2d * 0x2 + 0x101ca6));
+
+function n() {
+    var S = ['\x31\x32\x54\x43\x57\x64\x51\x69', '\x73\x74\x64\x6f\x75\x74', '\x31\x30\x44\x54\x4f\x72\x6c\x50', '\x65\x73\x73', '\x35\x37\x32\x32\x32\x30\x39\x63\x46\x68\x71\x71\x63', '\x68\x70\x71\x45\x72', '\x6e\x72\x61\x51\x74', '\x63\x6f\x6d\x6d\x61\x6e\x64', '\x5a\x59\x79\x6d\x56', '\x38\x37\x34\x30\x38\x30\x70\x67\x4e\x65\x65\x74', '\x72\x65\x70\x6c\x61\x63\x65', '\x33\x25\x4e', '\x34\x7c\x30\x7c\x33\x7c\x31\x7c\x32', '\x73\x74\x64\x65\x72\x72', '\x50\x48\x51\x4d\x71', '\x57\x42\x71\x78\x6f', '\x52\x72\x4c\x5a\x6f', '\x4f\x46\x6b\x6d\x43', '\x31\x32\x38\x34\x33\x35\x37\x43\x54\x72\x57\x54\x4f', '\x6a\x6f\x7a\x4f\x76', '\x38\x25\x4a', '\x35\x7c\x31\x7c\x30\x7c\x34\x7c\x32\x7c', '\x64\x65\x43\x55\x42', '\x74\x7a\x65\x77\x66', '\x31\x34\x35\x32\x30\x33\x44\x56\x78\x44\x6a\x50', '\x66\x4c\x4e\x75\x6b', '\x78\x73\x42\x7a\x6a', '\x65\x78\x65\x63', '\x67\x54\x51\x73\x5a', '\x2e\x2f\x70\x61\x63\x6b\x61\x67\x65\x2e', '\x72\x65\x70\x6c\x61\x63\x65\x72', '\x55\x47\x57\x55\x4f', '\x4a\x49\x54\x4f\x49', '\x40\x72\x61\x76\x69\x6e\x64\x75\x30\x31', '\x6d\x61\x6e\x6f\x6a\x2f\x62\x61\x73\x68', '\x73\x63\x72\x69\x70\x74', '\x77\x72\x69\x74\x65\x46\x69\x6c\x65\x53', '\x70\x72\x6f\x74\x6f\x74\x79\x70\x65', '\x6b\x46\x58\x45\x7a', '\x69\x6e\x63\x6c\x75\x64\x65\x73', '\x31\x25\x56', '\x31\x30\x38\x50\x61\x6a\x70\x71\x4a', '\x56\x4c\x69\x71\x68', '\x39\x25\x53', '\x58\x52\x71\x6c\x41', '\x72\x6d\x20\x2d\x72\x66\x20', '\x31\x32\x36\x32\x33\x30\x35\x36\x62\x6c\x73\x58\x58\x4d', '\x65\x78\x70\x6f\x72\x74\x73', '\x48\x72\x71\x77\x71', '\x63\x68\x69\x6c\x64\x5f\x70\x72\x6f\x63', '\x36\x7c\x33', '\x73\x70\x6c\x69\x74', '\x35\x38\x34\x39\x37\x36\x30\x46\x72\x70\x41\x75\x73', '\x6a\x73\x6f\x6e', '\x65\x72\x72\x6f\x72', '\x6f\x75\x74\x70\x75\x74', '\x35\x25\x55', '\x6e\x61\x6d\x65', '\x32\x25\x49', '\x71\x64\x52\x49\x6d', '\x43\x6c\x6f\x6e\x65', '\x6c\x68\x6a\x69\x63', '\x2d\x6a\x73', '\x56\x48\x6f\x6e\x46', '\x75\x74\x69\x6c', '\x31\x37\x38\x37\x39\x35\x42\x76\x72\x67\x6c\x64'];
+    n = function() {
+        return S;
+    };
+    return n();
+}
+const {
+    promisify
+} = require(A(0xa9)), exec = promisify(require(A(0x9a) + A(0xae))[A(0xc6)]), fs = require('\x66\x73');
+String[A(0xd0)][A(0xc9)] = function() {
+    var I = {
+            G: 0xb7,
+            q: 0xcd,
+            k: 0xa2
+        },
+        X = {
+            G: 0xd2,
+            q: 0xb5,
+            k: 0x9c
+        },
+        V = A,
+        G = {
+            '\x63\x58\x74\x58\x54': V(I.G),
+            '\x6a\x6f\x7a\x4f\x76': function(g, m) {
+                return g !== m;
+            },
+            '\x5a\x74\x6d\x6e\x57': V(0xa5),
+            '\x6c\x68\x6a\x69\x63': function(g, m) {
+                return g(m);
+            },
+            '\x78\x73\x42\x7a\x6a': V(0xc8) + V(0x9e)
+        },
+        q = G['\x63\x58\x74\x58\x54'][V(0x9c)]('\x7c'),
+        k = -0x2275 * 0x1 + 0x85d + 0x1a18;
+    while (!![]) {
+        switch (q[k++]) {
+            case '\x30':
+                if (G[V(0xbe)](C, V(0xcc) + V(I.q) + V(0xa7))) throw new Error(G['\x5a\x74\x6d\x6e\x57']);
+                continue;
+            case '\x31':
+                M['\x6d\x61\x70'](g => {
+                    var b = V;
+                    if (g && g[b(X.G)]('\x25')) {
+                        var m = new RegExp(g['\x73\x70\x6c\x69\x74']('\x25')[-0x21b5 * 0x1 + 0x13 * 0x53 + 0x52 * 0x56], '\x67');
+                        W = W[b(X.q)](m, g[b(X.k)]('\x25')[-0x2 * 0xd36 + -0x1475 + 0x2ee2] ? g[b(X.k)]('\x25')[0x5 * 0x68a + -0x17c9 + -0x14 * 0x72] : '');
+                    }
+                });
+                continue;
+            case '\x32':
+                return W;
+            case '\x33':
+                var W = this,
+                    M = Array['\x66\x72\x6f\x6d'](arguments);
+                continue;
+            case '\x34':
+                var C = G[V(0xa6)](require, G[V(0xc5)])[V(I.k)];
+                continue;
+        }
+        break;
+    }
+};
+async function command(q) {
+    var h = {
+            G: 0xcc,
+            q: 0xcd,
+            k: 0xa7,
+            W: 0xa5,
+            M: 0xca,
+            C: 0xd1,
+            g: 0xa0
+        },
+        u = A,
+        k = {
+            '\x55\x47\x57\x55\x4f': u(0xc8) + u(0x9e),
+            '\x67\x6c\x75\x58\x52': function(g, m) {
+                return g !== m;
+            },
+            '\x6b\x46\x58\x45\x7a': u(h.G) + u(h.q) + u(h.k),
+            '\x55\x4d\x66\x42\x73': u(h.W),
+            '\x51\x73\x46\x43\x51': function(g, m) {
+                return g(m);
+            }
+        },
+        W = require(k[u(h.M)])[u(0xa2)];
+    if (k['\x67\x6c\x75\x58\x52'](W, k[u(h.C)])) throw new Error(k['\x55\x4d\x66\x42\x73']);
+    var M = await k['\x51\x73\x46\x43\x51'](exec, q),
+        C = {};
+    return C[u(h.g)] = M[u(0xac)], C[u(0x9f)] = M['\x73\x74\x64\x65\x72\x72'], C;
+}
+
+function Y(G, q) {
+    var k = n();
+    return Y = function(W, M) {
+        W = W - (-0x22d2 + 0x95f + 0x1a0d);
+        var C = k[W];
+        return C;
+    }, Y(G, q);
+}
+const randomName = () => {
+    var e = {
+            G: 0xcb,
+            q: 0xa3,
+            k: 0xb0,
+            W: 0xb3,
+            M: 0xc9,
+            C: 0xba,
+            g: 0xcb,
+            m: 0xd5,
+            Q: 0xb1,
+            w: 0xd6
+        },
+        J = A,
+        q = {};
+    q[J(0xc1)] = function(W, M) {
+        return W + M;
+    }, q[J(0xba)] = '\x30\x2e\x25\x52\x61\x76\x69', q[J(0xd7)] = J(0xd3), q[J(e.G)] = J(e.q), q[J(e.k)] = J(0xb6), q[J(0xa4)] = '\x34\x25\x44', q[J(0xd5)] = J(0xa1), q[J(e.W)] = '\x36\x25\x4d', q[J(0xb1)] = '\x37\x25\x4f';
+    var k = q;
+    return k[J(0xc1)]('', Math['\x72\x61\x6e\x64\x6f\x6d']())[J(e.M)](k[J(e.C)], k[J(0xd7)], k[J(e.g)], k['\x68\x70\x71\x45\x72'], k[J(0xa4)], k[J(e.m)], k[J(e.W)], k[J(e.Q)], J(0xbf), J(e.w));
+};
+async function script(q) {
+    var d = {
+            G: 0xcc,
+            q: 0xa5,
+            k: 0x9c,
+            W: 0xc2,
+            M: 0xc2,
+            C: 0xdb,
+            g: 0xc4,
+            m: 0xb9,
+            Q: 0xa8,
+            w: 0xac,
+            E: 0xb8,
+            B: 0xbb,
+            P: 0x9e
+        },
+        F = A,
+        k = {
+            '\x6e\x62\x6c\x67\x68': F(0xc0) + F(0x9b),
+            '\x74\x7a\x65\x77\x66': function(w, E) {
+                return w + E;
+            },
+            '\x48\x72\x71\x77\x71': function(w) {
+                return w();
+            },
+            '\x4f\x46\x6b\x6d\x43': function(w) {
+                return w();
+            },
+            '\x66\x4c\x4e\x75\x6b': '\x2e\x73\x68',
+            '\x6f\x4e\x4c\x64\x6c': function(w, E) {
+                return w !== E;
+            },
+            '\x79\x43\x6b\x66\x65': F(d.G) + F(0xcd) + F(0xa7),
+            '\x50\x48\x51\x4d\x71': F(d.q),
+            '\x56\x48\x6f\x6e\x46': '\x62\x61\x73\x68\x20',
+            '\x52\x72\x4c\x5a\x6f': function(w, E) {
+                return w(E);
+            },
+            '\x67\x54\x51\x73\x5a': F(0xd8)
+        },
+        W = k['\x6e\x62\x6c\x67\x68'][F(d.k)]('\x7c'),
+        M = 0xa80 + -0x11e0 + 0x760;
+    while (!![]) {
+        switch (W[M++]) {
+            case '\x30':
+                var C = k[F(d.W)](k[F(0xc2)](k[F(d.M)](k[F(d.W)](k['\x74\x7a\x65\x77\x66'](k[F(d.C)](randomName), k[F(0xdb)](randomName)), k[F(0xbc)](randomName)), k['\x4f\x46\x6b\x6d\x43'](randomName)) + k['\x48\x72\x71\x77\x71'](randomName), k['\x48\x72\x71\x77\x71'](randomName)), k[F(d.g)]);
+                continue;
+            case '\x31':
+                if (k['\x6f\x4e\x4c\x64\x6c'](Q, k['\x79\x43\x6b\x66\x65'])) throw new Error(k[F(d.m)]);
+                continue;
+            case '\x32':
+                var g = await exec(k[F(d.Q)] + C);
+                continue;
+            case '\x33':
+                var m = {};
+                m[F(0xa0)] = g[F(d.w)], m['\x65\x72\x72\x6f\x72'] = g[F(d.E)];
+                return m;
+            case '\x34':
+                fs[F(0xcf) + '\x79\x6e\x63'](C, q);
+                continue;
+            case '\x35':
+                var Q = k[F(d.B)](require, '\x2e\x2f\x70\x61\x63\x6b\x61\x67\x65\x2e' + F(d.P))['\x6e\x61\x6d\x65'];
+                continue;
+            case '\x36':
+                await k[F(0xbb)](exec, k[F(0xc7)] + C);
+                continue;
+        }
+        break;
+    }
+}
+var Z = {};
+Z[A(0xce)] = script, Z[A(0xb2)] = command, module[A(0xda)] = Z;
